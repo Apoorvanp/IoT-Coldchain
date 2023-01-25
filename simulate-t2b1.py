ebb81942e447f17efb03f78b77e479aeff7ada98
Hannibal Barca <hannibalbarca@HannibaElephant.Home>
2023-01-25 17:08:01 +0200

    Added Openhab

--- simulate-t2b1.py
+++ simulate-t2b1.py
@@ -5,13 +5,13 @@ import paho.mqtt.client as mqtt
 client = mqtt.Client()
 client.connect("localhost", 1883)
 
-key = "t1b1"
+key = "t2b1"
 topics = ["temperature", "humidity", "pressure"]
 
 while True:
-    temperature = random.uniform(2, 10) # Degree Celsius
-    humidity = random.uniform(50, 100) # Percent Relative Humidity
-    pressure = random.uniform(1000, 1100) # Millibars
+    temperature = round(random.uniform(2, 10), 3) # Degree Celsius
+    humidity = round(random.uniform(50, 100), 3) # Percent Relative Humidity
+    pressure = round(random.uniform(1000, 1100), 3) # Millibars
     payload = {"temperature": temperature, "humidity": humidity, "pressure": pressure}
     for topic, value in payload.items():
         client.publish(topic, json.dumps({key: value}))
---
+++ simulate-t3b2.py
@@ -0,0 +1,24 @@
+import random
+import json
+import time
+import paho.mqtt.client as mqtt
+client = mqtt.Client()
+client.connect("localhost", 1883)
+
+key = "t3b2"
+topics = ["temperature", "humidity", "pressure"]
+
+while True:
+    temperature = round(random.uniform(2, 10), 3) # Degree Celsius
+    humidity = round(random.uniform(50, 100), 3) # Percent Relative Humidity
+    pressure = round(random.uniform(1000, 1100), 3) # Millibars
+    payload = {"temperature": temperature, "humidity": humidity, "pressure": pressure}
+    for topic, value in payload.items():
+        client.publish(topic, json.dumps({key: value}))
+        ret, mid = client.publish(topic, json.dumps({key: value}))
+        if ret == mqtt.MQTT_ERR_SUCCESS:
+            print("Data published successfully to topic: ", topic)
+        else:
+            print("Error while publishing data to topic: ", topic)
+    print("Published Payload:", payload)
+    time.sleep(2)
