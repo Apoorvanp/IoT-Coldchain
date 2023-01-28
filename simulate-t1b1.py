848f4617c9981d8e32a13dfa37200e4ed4ba7fb3
Apoorva NP <npapoorva1997@gmail.com>
2023-01-29 00:28:51 +0200

    Change json schema of published event

--- simulate-t1b1.py
+++ simulate-t1b1.py
@@ -1,12 +1,10 @@
 import random
-from flask import Flask, request
 import numpy as np
 import json
 import time
 import paho.mqtt.client as mqtt
 client = mqtt.Client()
 client.connect("localhost", 1883)
-# app = Flask(__name__)
 
 #PRESETS
 #PFIZER VACCINE
@@ -37,11 +35,6 @@ topics = ["temperature", "humidity", "pressure"]
 
 while True:
 
-    # @app.route('/cooling_soln', methods=['POST'])
-    # def set_cooling_soln():
-    #     cooling_soln = request.json['cooling_soln']
-    #     return '', 204
-
     temp_var=temp_delta*(random.random()*((temp_upperbound-temp_lowerbound)+temp_lowerbound)*100)/100
     humidity_var=humidity_delta*(random.random()*((humidity_upperbound-humidity_lowerbound)+humidity_lowerbound)*100)/100
     pressure_var=pressure_delta*(random.random()*((pressure_upperbound-pressure_lowerbound)+pressure_lowerbound)*100)/100
@@ -59,8 +52,7 @@ while True:
 
     payload = {"temperature": temperature, "humidity": humidity, "pressure": pressure}
     for topic, value in payload.items():
-        client.publish(topic, json.dumps({key: value}))
-        ret, mid = client.publish(topic, json.dumps({key: value}))
+        ret, mid = client.publish(topic, json.dumps({"id": key, "value": round(value, 2)}))
         if ret == mqtt.MQTT_ERR_SUCCESS:
             print("Data published successfully to topic: ", topic)
         else:
